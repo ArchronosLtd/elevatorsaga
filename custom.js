1441481175ac5062de68f6818c51ea679d706f41
Amit pattjoshi <amit.pattjoshi@gmail.com>
2020-05-03 00:45:44 +0100

    added the elevator solution and successfully ran upto 5 challenges - needs refreshing for failed attempt at times to success

--- custom.js
+++ custom.js
@@ -1,15 +1,62 @@
 var solution = {
-    init: function (elevators, floors) {
-        var elevator = elevators[0]; // Let's use the first elevator
+    init: function(elevators, floors) {
 
-        // Whenever the elevator is idle (has no more queued destinations) ...
-        elevator.on("idle", function () {
-            // let's go to all the floors (or did we forget one?)
-            elevator.goToFloor(0);
-            elevator.goToFloor(1);
-        });
+        elevators.forEach(function (elevator, currentElevatorIndex) {
+        var buttonsPressedOnElevator= []
+        elevator.on("idle", function(){
+            // if at least one request pending for btn pressed inside elevator 
+            if(buttonsPressedOnElevator.length > 0 ) {
+            console.log("buttonsPressedOnElevator ", buttonsPressedOnElevator)        
+            let indexOfFloorWithMinDistanceToTravel = -1
+            let lowestDistanceElevatorToTravel = 10000
+            buttonsPressedOnElevator.forEach((btnPressedOnElevator, index) => {
+                let distanceToBtnPressedFloor = Math.abs(btnPressedOnElevator - elevator.currentFloor());
+                        if(distanceToBtnPressedFloor < lowestDistanceElevatorToTravel) {
+                            lowestDistanceElevatorToTravel = distanceToBtnPressedFloor;
+                            indexOfFloorWithMinDistanceToTravel = index;
+                        }
+                    })
+                    elevator.goToFloor((buttonsPressedOnElevator.splice(indexOfFloorWithMinDistanceToTravel, 1))[0]);
+                }
+                // Else Elevator is Empty and will serve request from other floors waiting for elevator to be picked
+                else { 
+                    console.log("floorsAwaitingForElevatorStatus ", floorsAwaitingForElevatorStatus)        
+
+                    let lowestDistanceElevatorToTravel = 10000
+                    let indexOfFloorWithMinDistanceToTravel = currentElevatorIndex
+                    floorsAwaitingForElevatorStatus.forEach((floorAwaitingForElevatorStatus, floorNumIndex) => {
+                        if(floorAwaitingForElevatorStatus) {
+                            let distanceToAwaitingFloor = Math.abs(floorNumIndex - elevator.currentFloor());
+                            if(distanceToAwaitingFloor < lowestDistanceElevatorToTravel) {
+                                lowestDistanceElevatorToTravel = distanceToAwaitingFloor;
+                                indexOfFloorWithMinDistanceToTravel = floorNumIndex;
+                            }
+                        }
+                    })
+                    elevator.goToFloor(indexOfFloorWithMinDistanceToTravel);
+                }
+            })
+            elevator.on("floor_button_pressed", function(floorNum) {
+                buttonsPressedOnElevator.push(floorNum)
+            });
+            elevator.on("stopped_at_floor", function(floorNum){
+                floorsAwaitingForElevatorStatus[floorNum] = false;
+            })
+        })
+
+        var floorsAwaitingForElevatorStatus = []
+        floors.forEach(floor => {
+            floorsAwaitingForElevatorStatus.push(false);
+
+            floor.on("up_button_pressed", function() {
+                floorsAwaitingForElevatorStatus[floor.floorNum()] = true
+            });
+            floor.on("down_button_pressed", function() {
+                floorsAwaitingForElevatorStatus[floor.floorNum()] = true
+            })        
+         });
     },
-    update: function (dt, elevators, floors) {
+    update: function(dt, elevators, floors) {
         // We normally don't need to do anything here
+        }
     }
-}
